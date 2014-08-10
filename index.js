74b599fd18922c817d36fc6ae33faed56ea05853
Morgan Herlocker <morgan.herlocker@gmail.com>
2014-08-10 15:17:13 -0400

    add point test

--- index.js
+++ index.js
@@ -2,19 +2,21 @@ var s2Cover = require('geojson-cover'),
     tileCover = require('tile-cover'),
     fs = require('fs')
 
+console.log('Counties:')
+
 var counties = JSON.parse(fs.readFileSync('./fixtures/counties.geojson'))
 counties.features = counties.features.slice(0, 200)
-console.time('tiles')
+console.time('polygon tiles')
 
 counties.features.forEach(function(county){
 	console.time()
-  tileCover.indexes(county.geometry, {min_zoom:1, max_zoom: 8})
+  tileCover.indexes(county.geometry, {min_zoom:5, max_zoom: 5})
 });
 
-console.timeEnd('tiles')
+console.timeEnd('polygon tiles')
 
 
-console.time('cells')
+console.time('polygon cells')
 
 counties.features.forEach(function(county){
 try{
@@ -25,4 +27,33 @@ catch(err){
 }
 });
 
-console.timeEnd('cells')
+console.timeEnd('polygon cells')
+
+
+
+console.log('DC Drinking Fountains:')
+
+var dc_fountains = JSON.parse(fs.readFileSync('./fixtures/dc_fountains.geojson'))
+dc_fountains.features = dc_fountains.features.slice(0, 200)
+console.time('point tiles')
+
+counties.features.forEach(function(fountain){
+  console.time()
+  tileCover.indexes(fountain.geometry, {min_zoom:5, max_zoom: 5})
+});
+
+console.timeEnd('point tiles')
+
+
+console.time('point cells')
+
+counties.features.forEach(function(fountain){
+try{
+  s2Cover.geometryIndexes(fountain.geometry)
+}
+catch(err){
+//console.log('invalid loop')
+}
+});
+
+console.timeEnd('point cells')
