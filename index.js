1614dd755387b49172e342681082a84b320a345f
Tom MacWright <tom@macwright.org>
2014-08-11 14:28:53 -0400

    Use benchmark for better stat-driven results.

--- index.js
+++ index.js
@@ -1,36 +1,57 @@
 var s2Cover = require('geojson-cover'),
     tileCover = require('tile-cover'),
-    fs = require('fs')
+    queue = require('queue-async'),
+    Benchmark = require('benchmark'),
+    fs = require('fs');
 
-console.log('Counties:')
+global.tileCover = tileCover;
+global.s2Cover = s2Cover;
+global.fs = fs;
 
-var counties = JSON.parse(fs.readFileSync('./fixtures/counties.geojson'))
-counties.features = counties.features.slice(0, 200)
-console.time('polygon tiles')
-
-counties.features.forEach(function(county){
-	console.time()
-  tileCover.indexes(county.geometry, {min_zoom:1, max_zoom: 5})
-});
-
-console.timeEnd('polygon tiles')
-
-
-console.time('polygon cells')
-
-counties.features.forEach(function(county){
-try{
-  s2Cover.geometryIndexes(county.geometry)
+function testData(file, callback) {
+    console.log('\n' + file + '\n');
+    global.file = file;
+    var suite = new Benchmark.Suite(file);
+    suite
+    .add({
+        name: 'tileCover.indexes',
+        fn: function() {
+            counties.features.forEach(function(county){
+              global.tileCover.indexes(county.geometry, {min_zoom:1, max_zoom: 5})
+            });
+        },
+        setup: function() {
+            var counties = JSON.parse(global.fs.readFileSync(global.file));
+            counties.features = counties.features.slice(0, 2)
+        }
+    })
+    .add({
+        name: 's2.indexes',
+        fn: function() {
+            counties.features.forEach(function(county){
+              global.s2Cover.geometryIndexes(county.geometry)
+            });
+        },
+        setup: function() {
+            var counties = JSON.parse(global.fs.readFileSync(global.file));
+            counties.features = counties.features.slice(0, 2)
+        }
+    })
+    .on('cycle', function(event) {
+      console.log(String(event.target));
+    })
+    .on('complete', function() {
+      console.log('Fastest is ' + this.filter('fastest').pluck('name'));
+      callback();
+    })
+    .run();
 }
-catch(err){
-//console.log('invalid loop')
-}
-});
 
-console.timeEnd('polygon cells')
+queue(1)
+    .defer(testData, './fixtures/dc_fountains.geojson')
+    .defer(testData, './fixtures/counties.geojson');
 
-
-
+/*
 console.log('DC Drinking Fountains:')
 
 var dc_fountains = JSON.parse(fs.readFileSync('./fixtures/dc_fountains.geojson'))
@@ -57,3 +78,4 @@ catch(err){
 });
 
 console.timeEnd('point cells')
+*/
